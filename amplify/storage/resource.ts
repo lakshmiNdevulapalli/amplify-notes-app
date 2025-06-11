ec7b2490712357cb5df486e044549aaaecbd5291
baludevulapalli <baludevulapalli@gmail.com>
2025-06-11 15:51:45 -0700

    Added amplify yml

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,14 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+/**
+ * Define and configure your storage resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/storage
+ */
+export const storage = defineStorage({
+	name: "ampllifyNotesDrive",
+	access: (allow) => ({
+		"media/{entity_id}/*": [
+			allow.entity("identity").to(["read", "write", "delete"]),
+		],
+	}),
+});
